cff5a2ea90beae679f8b5d98d273922a8da7a011
Jeff Benson <blaster151@gmail.com>
2018-02-25 14:57:17 -0600

    Added and verified http dependency

--- ang/src/app/app.component.ts
+++ ang/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private fromService: any = null;
+  constructor(private http: Http)
+  {
+    http.get('http://localhost:3000').subscribe(rsp => {
+      console.log('got response from Nest');
+      console.log(rsp.json());
+      this.fromService = rsp.json();
+    });
+  }
+
   title = 'app';
 }
