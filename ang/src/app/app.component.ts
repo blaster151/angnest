483419c3aa30a8b88303597b063d133b3cee3cdf
Jeff Benson <blaster151@gmail.com>
2018-02-25 22:11:05 -0600

    Displaying branch info

--- ang/src/app/app.component.ts
+++ ang/src/app/app.component.ts
@@ -9,13 +9,17 @@ import { run } from '../../../shared/common';
 })
 export class AppComponent {
   private fromService: any = null;
+  private branches: string[] = [];
+
   constructor(private http: Http)
   {
-    http.get('http://localhost:3000').subscribe(rsp => {
+    http.get('http://localhost:3000/branches').subscribe(rsp => {
       console.log('got response from Nest');
       console.log(rsp.json());
-      this.fromService = rsp.json();
+      this.fromService = rsp.json().current;
+      this.branches = Object.keys(rsp.json().branches).map(k => rsp.json().branches[k].name);
 
+      
       
       run();
     });
